009d7701649f4a9634d5452de8717406916e8187
João Pedro Martins Forte <jpforte2004@gmail.com>
2024-07-11 20:49:57 -0300

    Salva dados localmente e exibe na tabela

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const formulario = document.querySelector('.js-form');
 const campoNome = document.querySelectorAll('.js-field')[0];
 const campoData = document.querySelectorAll('.js-field')[1];
+const tbody = document.querySelector('.js-tbody');
+const trNenhuma = document.querySelector('.js-nenhuma');
 
 const tamanhoMinimoNome = 3;
 const tamanhoMaximoNome = 120;
+
+let pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
 
 campoNome.setAttribute('minlength', tamanhoMinimoNome);
 campoNome.setAttribute('maxlength', tamanhoMaximoNome);
 campoData.setAttribute('max', new Date().toISOString().split('T')[0]);
 
+const criarElemento = (pessoa) => {
+    const tr = document.createElement('tr');
+    const tdNome = document.createElement('td');
+    tdNome.textContent = pessoa.nome;
+    const tdDataNascimento = document.createElement('td');
+    tdDataNascimento.textContent = pessoa.dataNascimento;
+    tr.appendChild(tdNome);
+    tr.appendChild(tdDataNascimento);
+    return tr;
+}
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
+
     const nome = campoNome.value;
-    let data = campoData.value.split('-');
-    data = `${data[2]}/${data[1]}/${data[0]}`;
-    console.log(`Nome: ${nome}\nAniversário: ${data}`);
+    let dataNascimento = campoData.value.split('-');
+    dataNascimento = `${dataNascimento[2]}/${dataNascimento[1]}/${dataNascimento[0]}`;
+
+    const pessoa = { nome, dataNascimento };
+    pessoas.push(pessoa);
+    localStorage.setItem("pessoas", JSON.stringify(pessoas));
+
+    trNenhuma.remove();
+    const elemento = criarElemento(pessoa);
+    tbody.appendChild(elemento);
+
+    campoNome.value = '';
+    campoData.value = '';
 });
 
 campoNome.oninvalid = () => {
@@ -29,4 +54,12 @@ campoNome.oninvalid = () => {
     else if(!campoNome.validity.valid){
         campoNome.setCustomValidity("Nome inválido.");
     }
+}
+
+if(pessoas.length > 0){
+    trNenhuma.remove();
+    pessoas.forEach(pessoa => {
+        const elemento = criarElemento(pessoa);
+        tbody.appendChild(elemento);
+    });
 }
