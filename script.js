0a4982930c61f241a1c6ac74ab96aceaf64c61a6
João Pedro Martins Forte <jpforte2004@gmail.com>
2024-07-11 19:46:30 -0300

    Validações no formulário

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const formulario = document.querySelector('.js-form');
-const camposFormulario = document.querySelectorAll('.js-field');
+const campoNome = document.querySelectorAll('.js-field')[0];
+const campoData = document.querySelectorAll('.js-field')[1];
+
+const tamanhoMinimoNome = 3;
+const tamanhoMaximoNome = 120;
+
+campoNome.setAttribute('minlength', tamanhoMinimoNome);
+campoNome.setAttribute('maxlength', tamanhoMaximoNome);
+campoData.setAttribute('max', new Date().toISOString().split('T')[0]);
+
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
-    const nome = camposFormulario[0].value;
-    const data = camposFormulario[1].value;
+    const nome = campoNome.value;
+    let data = campoData.value.split('-');
+    data = `${data[2]}/${data[1]}/${data[0]}`;
     console.log(`Nome: ${nome}\nAniversário: ${data}`);
 });
+
+campoNome.oninvalid = () => {
+    campoNome.setCustomValidity("");
+    if(!campoNome.validity.valid && campoNome.value.length < tamanhoMinimoNome){
+        campoNome.setCustomValidity(`O nome precisa ter no mínimo ${tamanhoMinimoNome} letras.`);
+    }
+    else if(!campoNome.validity.valid && campoNome.value.length > tamanhoMaximoNome){
+        campoNome.setCustomValidity(`O nome precisa ter no máximo ${tamanhoMaximoNome} letras.`);
+    }
+    else if(!campoNome.validity.valid){
+        campoNome.setCustomValidity("Nome inválido.");
+    }
+}
